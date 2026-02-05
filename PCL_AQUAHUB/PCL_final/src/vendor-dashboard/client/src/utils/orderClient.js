41e9c47f3aa5d44c101d4132b9e2577cd3f641b7
RayyanSameer <turbocharged.tomato@gmail.com>
2026-02-05 05:11:11 +0000

    Restore .gitmodules and prepare PCL_AQUAHUB for submodule

--- PCL_AQUAHUB/PCL_final/src/vendor-dashboard/client/src/utils/orderClient.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const client = axios.create({
-  baseURL: 'http://localhost:4000/api/orders'
-})
-
-export default client
--- PCL_AQUAHUB/PCL_final/src/vendor-dashboard/src/db/database.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('aquahub_vendor.db');
-
-// Enable foreign keys
-db.pragma('foreign_keys = ON');
-
-module.exports = db;
